dd846a5223b7ed573888df7148798004ecad1ea2
Christopher Clarke <afroradiohead@gmail.com>
2017-10-13 18:26:38 -0700

    add more product

--- server/src/model/product.ts
+++ server/src/model/product.ts
@@ -10,5 +10,34 @@ export const productList: IProduct[] = [
       original: 143.00,
       discount: 280.00
     },
+  },
+  {
+    image: 'https://images-na.ssl-images-amazon.com/images/I/61y0vonCSbL._SX215_.jpg',
+    name: 'Lego Batman - Nintendo DS',
+    link: 'https://www.amazon.com/Lego-Battles-Ninjago-Nintendo-DS/dp/B004IYY8PW/ref=sr_1_1?s=videogames&ie=UTF8&qid=1507834556&sr=1-1&keywords=Nintendo+ds+game',
+    slug: 'lego-batman-nintendo-ds',
+    price: {
+      original: 143.00,
+      discount: 280.00
+    },
+  },
+  {
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51NHmy-KzUL._SX215_.jpg',
+    name: 'LEGO Batman 2: DC Super Heroes ',
+    link: 'https://www.amazon.com/Lego-Battles-Ninjago-Nintendo-DS/dp/B004IYY8PW/ref=sr_1_1?s=videogames&ie=UTF8&qid=1507834556&sr=1-1&keywords=Nintendo+ds+game',
+    slug: 'lego-batman-2-dc-super-heroes-nintendo-ds',
+    price: {
+      original: 113.00,
+      discount: 280.00
+    },
+  },
+  {
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51Mb51qDhzL._SX215_.jpg',
+    name: 'Teenage Mutant Ninja Turtles ',
+    link: 'teenage-mutant-ninja-turtles',
+    price: {
+      original: 113.00,
+      discount: 280.00
+    },
   }
 ];
