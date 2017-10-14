f1c1e3355445af61dd04fcd8793c98af20a9ba7b
Christopher Clarke <afroradiohead@gmail.com>
2017-10-13 22:09:41 -0700

    remove unused classes

--- shared/socketer/product-page.ts
+++ shared/socketer/product-page.ts
@@ -1,18 +1,6 @@
 import {IProduct} from '../interface/product';
 import {ASocketEvent, Socketeer} from "./index";
 
-
-interface IInitRequest {
-  slug: string;
-}
-interface IInitResponse {
-  product: IProduct;
-}
-export class InitSocketeer extends Socketeer<IInitRequest, IInitResponse> {
-  request_event = 'product-page/init';
-  response_event = 'product-page/init.response';
-}
-
 export class SocketEvent_Init_FromClient extends ASocketEvent<{
   slug: string;
 }> {
