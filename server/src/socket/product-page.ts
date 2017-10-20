525bb035fc56ba2b56c02d3483eb71e42f03a7ec
Christopher Clarke <afroradiohead@gmail.com>
2017-10-20 15:24:56 -0700

    try to fix crashing error

--- server/src/socket/product-page.ts
+++ server/src/socket/product-page.ts
@@ -11,28 +11,28 @@ export class ProductPageEndpoint {
     socketeer.from('INIT_FROMCLIENT').subscribe(request => {
       const db = HostDatabase.Create();
 
-      Observable.fromPromise(db.connect())
-        .mergeMap(() => {
-          return Observable.fromPromise(db.Products.findOne({
-            slug: request.slug.toLowerCase().trim(),
-            domain: socket.handshake.query.domain
-          })).combineLatest(
-            Observable.fromPromise(db.Products.aggregate([
-              {$match: {domain: socket.handshake.query.domain, slug: {$ne: request.slug}}},
-              {$sample: { size: 3 }}
-            ]))
-          );
-        })
-        .subscribe(data => {
-          const product = data[0];
-          const randomProductList = data[1] as IProduct[];
-
-          socketeer.send('INIT_FROMSERVER', {
-            product: product.toJSON(),
-            randomProductList: randomProductList
-          });
-          db.close();
-        });
+      // Observable.fromPromise(db.connect())
+      //   .mergeMap(() => {
+      //     return Observable.fromPromise(db.Products.findOne({
+      //       slug: request.slug.toLowerCase().trim(),
+      //       domain: socket.handshake.query.domain
+      //     })).combineLatest(
+      //       Observable.fromPromise(db.Products.aggregate([
+      //         {$match: {domain: socket.handshake.query.domain, slug: {$ne: request.slug}}},
+      //         {$sample: { size: 3 }}
+      //       ]))
+      //     );
+      //   })
+      //   .subscribe(data => {
+      //     const product = data[0];
+      //     const randomProductList = data[1] as IProduct[];
+      //
+      //     socketeer.send('INIT_FROMSERVER', {
+      //       product: product.toJSON(),
+      //       randomProductList: randomProductList
+      //     });
+      //     db.close();
+      //   });
     });
   }
 }
