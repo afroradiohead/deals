acd800b2a5be1e2a52dfc797dd244a5bc5ab8ac4
Christopher Clarke <afroradiohead@gmail.com>
2017-10-24 13:52:00 -0700

    Revert "add snapsearch"

--- _server.js
+++ _server.js
@@ -1,5 +1,4 @@
 // server.js
-const snapsearch = require('snapsearch-client-nodejs');
 const axios = require('axios');
 const express = require('express');
 const path = require('path');
@@ -7,33 +6,31 @@ const app = express();
 const server = require('./server/dist/server/src');
 const httpServer = app.listen(process.env.PORT || 8080);
 
+app.use(require('prerender-node').set('prerenderToken', 'nfzgLQblqGsuWNQ9k0vY'));
 app.set("httpServer", httpServer);
-
-
-// app.use(snapsearch.connect(
-//   new snapsearch.Interceptor(
-//     new snapsearch.Client('ENTER YOUR EMAIL', 'ENTER YOUR KEY', {}, function (error, debugging) {
-//       //mandatory custom exception handler for Client errors such as HTTP errors or validation errors from the API
-//       console.log(error);
-//       // error is a SnapSearchException containing a message and errorDetails which can acquired from `getMessage()` `getErrors()`
-//       console.log(debugging);
-//       // debugging is an object containing these: {apiUrl, apiKey, apiEmail, requestParameters}
-//       // if an exception happens, the middleware is a no-op and passes through to the next stage of your application
-//     }),
-//     new snapsearch.Detector()
-//   )
-// ));
 
 new server.AppServer(app);
 
 
 const initialRequest = function(req, res) {
-  try {
-    const snapSearchClient = new snapsearch.Client('afroradiohead@gmail.com', 'oSK8qca348m1RNC6f207ILt0Mz7pb4126MFHpR83thrTHkQamV', {});
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 
+  try {
+    axios.post('http://api.prerender.io/recache', {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        prerenderToken: "nfzgLQblqGsuWNQ9k0vY",
+        url: `${req.protocol}://${req.headers.host}${req.originalUrl}`
+      }
+    }).then(e => {
+      console.log('prerender worked');
+    }).catch(ex => {
+      console.log(ex.response.data)
+    });
   } catch(ex) {
-    console.log('snapsearch threw an error');
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    console.log('could not send request cache from prerender.io - 2')
   }
 };
 
