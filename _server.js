72752f2e4c2077e05fd74a37cb0e75e4a50ff3fa
Christopher Clarke <afroradiohead@gmail.com>
2017-10-24 12:59:16 -0700

    use prerender possibly better

--- _server.js
+++ _server.js
@@ -7,25 +7,36 @@ const server = require('./server/dist/server/src');
 const httpServer = app.listen(process.env.PORT || 8080);
 
 app.use(require('prerender-node').set('prerenderToken', 'nfzgLQblqGsuWNQ9k0vY'));
-
-app.use(express.static(__dirname + '/dist'))
-  .set("httpServer", httpServer);
+app.set("httpServer", httpServer);
 
 new server.AppServer(app);
 
-app.get('*', function (req, res) {
+
+const initialRequest = function(req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 
   try {
     axios.post('http://api.prerender.io/recache', {
+      headers: {
+        'Content-Type': 'application/json'
+      },
       data: {
-        "prerenderToken": "nfzgLQblqGsuWNQ9k0vY",
-        "url": `${req.protocol}://${req.headers.host}${req.originalUrl}`
+        prerenderToken: "nfzgLQblqGsuWNQ9k0vY",
+        url: `${req.protocol}://${req.headers.host}${req.originalUrl}`
       }
+    }).then(e => {
+      console.log('prerender worked');
     }).catch(ex => {
-      console.log('could not send request cache from prerender.io - 1')
+      console.log(ex.response.data)
     });
   } catch(ex) {
     console.log('could not send request cache from prerender.io - 2')
   }
-});
+};
+
+app.get('/', initialRequest);
+// @todo fix the way this look, cuz it's weird that i gotta duplicate code
+app
+  .use(express.static(__dirname + '/dist'))
+  .get('*', initialRequest);
+
