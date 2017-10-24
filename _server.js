ccd78c27be22588776889fbff9dabb54f9876c2f
Christopher Clarke <afroradiohead@gmail.com>
2017-10-24 12:26:18 -0700

    send request to prerender manually

--- _server.js
+++ _server.js
@@ -1,16 +1,31 @@
 // server.js
+const axios = require('axios');
 const express = require('express');
 const path = require('path');
 const app = express();
 const server = require('./server/dist/server/src');
+const httpServer = app.listen(process.env.PORT || 8080);
 
 app.use(require('prerender-node').set('prerenderToken', 'nfzgLQblqGsuWNQ9k0vY'));
 
-const httpServer = app.listen(process.env.PORT || 8080);
 app.use(express.static(__dirname + '/dist'))
   .set("httpServer", httpServer);
+
 new server.AppServer(app);
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist/index.html'))
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+
+  try {
+    axios.post('http://api.prerender.io/recache', {
+      data: {
+        "prerenderToken": "nfzgLQblqGsuWNQ9k0vY",
+        "url": `${req.protocol}://${req.headers.host}${req.originalUrl}`
+      }
+    }).catch(ex => {
+      console.log('could not send request cache from prerender.io - 1')
+    });
+  } catch(ex) {
+    console.log('could not send request cache from prerender.io - 2')
+  }
 });
