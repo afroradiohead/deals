352a5b660a823962ef0475f35bdcbfec6c5e24c9
Christopher Clarke <afroradiohead@gmail.com>
2017-11-01 16:50:06 -0700

    prep for snapshot seo

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,8 @@
 // server.js
 const request = require( 'request' );
 const _ = require( 'lodash' );
-const compression = require('compression')
+const compression = require('compression');
+const fs = require('fs');
 const seoBotDetect = require('seo-bot-detect');
 const express = require('express');
 const path = require('path');
@@ -13,19 +14,62 @@ const ___distdirname = `${__dirname}/../dist/`;
 app.set('httpServer', httpServer);
 app.use(compression());
 
+function RemoveParameterFromUrl(url, parameter) {
+  return url
+    .replace(new RegExp('[?&]' + parameter + '=[^&#]*(#.*)?$'), '$1')
+    .replace(new RegExp('([?&])' + parameter + '=[^&]*&'), '$1');
+}
+
 new server.AppServer(app);
 
-
 const initialRequest = function(req, res) {
-  res.sendFile(path.join(___distdirname, 'index.html'));
+  const requestedRendered = _.has(req.query, 'rendered');
+  const html = fs.readFileSync(path.join(___distdirname, 'index.html'));
+  // @todo append canonical url to head in html
+
+  if (requestedRendered) {
+    try {
+      request({
+        method: 'POST',
+        url: 'https://snapsearch.io/api/v1/robot',
+        auth: {
+          user: 'afroradiohead@gmail.com',
+          pass: 'oSK8qca348m1RNC6f207ILt0Mz7pb4126MFHpR83thrTHkQamV'
+        },
+        timeout: 2000,
+        json: {
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}`
+        },
+        strictSSL: true,
+        gzip: true
+      }, ( error, response, body ) => {
+        const cachedHtml = _.get(body, 'content.html', null);
+
+        if (cachedHtml) {
+          res.send(cachedHtml);
+        } else {
+          res.send(html);
+        }
+        res.end();
+      });
+    } catch (ex) {
+      res.send(html);
+      res.end();
+    }
+  } else {
+    res.send(html);
+    res.end();
+  }
 };
 
 app.get('/', initialRequest)
-  .get('/delay', (req, res) => {
-    setTimeout(() => {
-      res.send(`document.documentElement.classList.add('show');`);
-      res.end();
-    }, 3000);
+  .get('/rendered-direct', (req, res) => {
+    const redirectedUrl = RemoveParameterFromUrl(req.header('Referer'), 'rendered');
+
+    if (!seoBotDetect(req)) {
+        res.send(`window.location.href = "${redirectedUrl}";`);
+    }
+    res.end();
   })
   .get('/robots.txt', (req, res) => {
     res.sendFile(path.join(___distdirname, 'assets/robots.txt'));
