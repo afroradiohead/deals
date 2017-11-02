16f2724adc4896be9322ed866e59dd4863fc1ade
Christopher Clarke <afroradiohead@gmail.com>
2017-11-01 17:42:05 -0700

    prefill html with cached version

--- src/main.server.ts
+++ src/main.server.ts
@@ -3,7 +3,7 @@ const request = require( 'request' );
 const _ = require( 'lodash' );
 const compression = require('compression');
 const fs = require('fs');
-const seoBotDetect = require('seo-bot-detect');
+const cheerio = require('cheerio');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -14,68 +14,48 @@ const ___distdirname = `${__dirname}/../dist/`;
 app.set('httpServer', httpServer);
 app.use(compression());
 
-function RemoveParameterFromUrl(url, parameter) {
-  return url
-    .replace(new RegExp('[?&]' + parameter + '=[^&#]*(#.*)?$'), '$1')
-    .replace(new RegExp('([?&])' + parameter + '=[^&]*&'), '$1');
-}
 
 new server.AppServer(app);
 
 const initialRequest = function(req, res) {
   const requestedRendered = _.has(req.query, 'rendered');
-  const html = fs.readFileSync(path.join(___distdirname, 'index.html'));
-  // @todo append canonical url to head in html
-
+  const $html = cheerio.load(fs.readFileSync(path.join(___distdirname, 'index.html')));
 
   res.set('Content-Type', 'text/html');
 
-  if (requestedRendered) {
-    try {
-      request({
-        method: 'POST',
-        url: 'https://snapsearch.io/api/v1/robot',
-        auth: {
-          user: 'afroradiohead@gmail.com',
-          pass: 'oSK8qca348m1RNC6f207ILt0Mz7pb4126MFHpR83thrTHkQamV'
-        },
-        timeout: 2000,
-        json: {
-          url: `${req.protocol}://${req.headers.host}${req.originalUrl}`
-        },
-        strictSSL: true,
-        gzip: true
-      }, ( error, response, body ) => {
-        const cachedHtml = _.get(body, 'content.html', null);
+  try {
+    request({
+      method: 'POST',
+      url: 'https://snapsearch.io/api/v1/robot',
+      auth: {
+        user: 'afroradiohead@gmail.com',
+        pass: 'oSK8qca348m1RNC6f207ILt0Mz7pb4126MFHpR83thrTHkQamV'
+      },
+      timeout: 2000,
+      json: {
+        url: `${req.protocol}://${req.headers.host}${req.originalUrl}`
+      },
+      strictSSL: true,
+      gzip: true
+    }, ( error, response, body ) => {
+      const cachedHtml = _.get(body, 'content.html', null);
 
-        if (cachedHtml) {
-          res.send(cachedHtml);
-        } else {
-          res.send(html);
-        }
-        res.end();
-      });
-    } catch (ex) {
-      res.send(html);
+      if (cachedHtml) {
+        const $cachedHtml = cheerio.load(cachedHtml);
+        $html('app-root').html($cachedHtml('app-root').html());
+      }
+
+
+      res.send($html.html());
       res.end();
-    }
-  } else {
-    res.send(html);
+    });
+  } catch (ex) {
+    res.send($html.html());
     res.end();
   }
 };
 
 app.get('/', initialRequest)
-  .get('/rendered-direct', (req, res) => {
-    const redirectedUrl = RemoveParameterFromUrl(req.header('Referer'), 'rendered');
-
-    console.log(req.query);
-
-    // if (!seoBotDetect(req)) {
-    //     res.send(`window.location.href = "${redirectedUrl}";`);
-    // }
-    res.end();
-  })
   .get('/robots.txt', (req, res) => {
     res.sendFile(path.join(___distdirname, 'assets/robots.txt'));
   });
