f31bdcd841a2812dd035cbbfa8ae81337e439698
Christopher Clarke <afroradiohead@gmail.com>
2017-11-14 16:03:57 -0800

    clean up socketeer

--- src/app/home-page/home-page.socket.command.ts
+++ src/app/home-page/home-page.socket.command.ts
@@ -1,6 +1,5 @@
 import {IProduct} from '../../shared/interface/product';
-import {ISocketCommand} from '../../shared/class/socketeer';
-import {Observable} from "rxjs/Observable";
+import {ASocketeer, ISocketCommand} from '../../shared/class/socketeer';
 
 export class SocketCommand extends ISocketCommand {
   namespace = 'home-page';
@@ -16,42 +15,8 @@ export class SocketCommand extends ISocketCommand {
   };
 }
 
-
-// preparation for new socketeer
-interface ISocketCommand2 {
-  readonly CLIENT_EVENTS: {[event_name: string]: any};
-  readonly SERVER_EVENTS: {[event_name: string]: any};
-}
-
-export abstract class ASocketCommand2<T extends ISocketCommand2> {
-  readonly namespace: string;
-
-  constructor(private socket: any) {
-
-  }
-
-  toClient<K extends keyof T['CLIENT_EVENTS']>(key: K, value: T['CLIENT_EVENTS'][K]) {
-    this.socket.emit(`${this.namespace}/${key}`, value);
-  }
-
-  toServer<K extends keyof T['SERVER_EVENTS']>(key: K, value: T['SERVER_EVENTS'][K]) {
-    this.socket.emit(`${this.namespace}/${key}`, value);
-  }
-
-  fromClient<K extends keyof T['CLIENT_EVENTS']>(key: K): Observable<T['CLIENT_EVENTS'][K]> {
-    return Observable.create(observer => {
-      this.socket.on(`${this.namespace}/${key}`, (v) => observer.next(v));
-    });
-  }
-
-  fromServer<K extends keyof T['SERVER_EVENTS']>(key: K): Observable<T['SERVER_EVENTS'][K]> {
-    return Observable.create(observer => {
-      this.socket.on(`${this.namespace}/${key}`, (v) => observer.next(v));
-    });
-  }
-}
-
-export class SocketCommand2 extends ASocketCommand2<{
+// @todo use this sockeeter
+export class Socketeer extends ASocketeer<{
   CLIENT_EVENTS: {
     INIT: {
       time?: boolean;
