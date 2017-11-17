1f645664cf7d0043677e4583dcac977032b4f74f
Christopher Clarke <afroradiohead@gmail.com>
2017-11-16 20:24:58 -0800

    pass in x amount of arguments for gtag

--- src/app/shared/service/google-analytics.service.ts
+++ src/app/shared/service/google-analytics.service.ts
@@ -5,7 +5,7 @@ import {Socketeer} from "./google-analytics.socketeer";
 const ga = window['ga'] = window['ga'] || function(){(ga.q = ga.q || []).push(arguments); }; ga.l = +new Date;
 
 window['dataLayer'] = window['dataLayer'] || [];
-function gtag() {window['dataLayer'].push(arguments); }
+function gtag(...data) {window['dataLayer'].push(data); }
 
 
 
